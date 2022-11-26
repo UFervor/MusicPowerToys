2208784b5ae08a3d6db65a80eaa81420b1b9989f
UFervor <micsof365@gmail.com>
2022-11-26 22:41:12 +0800

    2022.11.26.b

--- AudioTool.py
+++ AudioTool.py
@@ -9,6 +9,7 @@ class Audio:
     def __init__(self, path, delimiter):
         self.path = os.path.abspath(path)
         self.delimiter = delimiter
+        self.dir = os.path.dirname(self.path)
 
     @property
     def Title(self):
@@ -25,7 +26,7 @@ class Audio:
                         break
             except:
                 pass
-        return title
+        return title.strip()
 
     @property
     def Album(self):
@@ -42,7 +43,7 @@ class Audio:
                         break
             except:
                 pass
-        return album
+        return album.strip()
 
     @property
     def Artist(self):
@@ -70,50 +71,41 @@ class Audio:
             artist = fin
         return artist
 
+    @property
+    def FileName(self):
+        return " - ".join(list(filter(None, [', '.join(self.Artist), self.Album, self.Title])))
+
 
 class AudioTool:
-    def __init__(self, base, suffix=["mp3", "flac", "aif"], delimiter=['&', '/', 'ft.', 'feat.', '\\\\']):
-        self.base = base.strip(["/", "\\"])
+    def __init__(self, base, suffix=["mp3", "flac", "aif"], delimiter=['&', '/', 'ft.', 'feat.', '\\\\'], limit=8):
+        self.base = base.strip("/\\")
         self.suffix = suffix
         self.delimiter = delimiter
+        self.audios = {}
+        self.artists = {}
+        self.limit = limit
 
     def fjoin(self, dir, name):
-        return self.base + dir + "\\" + name
+        return self.base + "\\" + dir + "\\" + name
 
     def sed(self, au):
-        album = au.Album
-        title = au.Title
-
+        # 歌手分割
         try:
             audio = FLAC(au.path)
             audio['ARTIST'] = '\\\\'.join(au.Artist)
             audio.save()
         except:
             tags = ID3(au.path)
-            tags['TPE1'] = TPE1(  # 插入第一演奏家、歌手、等
+            tags['TPE1'] = TPE1(
                 encoding=3,
                 text='\\\\'.join(au.Artist)
             )
             tags.save(au.path)
-        fname = ''
 
-        try:
-            fname += ', '.join(au.Artist)
-        except:
-            pass
-        if album != '':
-            if fname != '':
-                fname += ' - ' + album
-            else:
-                fname += album
+        dirname = " - ".join(list(filter(None,
+                             [', '.join(self.artists[au.Album]), au.Album])))
 
-        dirname = fname
-        if title != '':
-            if fname != '':
-                fname += ' - ' + title
-            else:
-                fname += title
-
+        fname = au.FileName
         lrcname = fname + '.lrc'
         fname += os.path.splitext(au.path)[-1]
         fname = fname.replace('/', '／').replace(':',
@@ -121,32 +113,54 @@ class AudioTool:
 
         if dirname not in os.listdir():
             try:
-                os.mkdir(dirname)
-                # print('Successfully Made Dir:' + dirname)
+                os.mkdir(self.base + "\\" + dirname)
+                print('Successfully Made Dir:' + dirname)
             except Exception as e:
                 print('Unexpected Error Occurred While Making Dir: ' + dirname)
                 print(e)
 
         try:
-            os.rename(au.path, fname)
-            # print('Successfully Renamed '+ src +' as '+ fname)
+            os.rename(au.path, au.dir + "\\" + fname)
+            print('Successfully Renamed ' + au.path +
+                  ' as ' + au.dir + "\\" + fname)
         except Exception as e:
             print('Unexpected Error Occurred While Renaming File: ' + au.path)
             print(e)
 
         try:
-            shutil.move(fname, self.fjoin(dirname, fname))
-            # print('Successfully Moved Song: '+ src)
-            if os.path.exists(lrcname):
-                shutil.move(lrcname, self.fjoin(dirname, lrcname))
-                # print('Successfully Moved Lrc: '+ lrcname)
+            shutil.move(au.dir + "\\" + fname, self.fjoin(dirname, fname))
+            print('Successfully Moved Song: ' + au.path)
+            if os.path.exists(au.dir + "\\" + lrcname):
+                shutil.move(au.dir + "\\" + lrcname,
+                            self.fjoin(dirname, lrcname))
+                print('Successfully Moved Lrc: ' + lrcname)
         except Exception as e:
             print('Unexpected Error Occurred While Moiving File: ' + au.path)
             print(e)
 
-        def start(self):
-            for root, ds, fs in os.walk(self.base):
-                for f in fs:
-                    if f[f.rfind(".") + 1:] in self.suffix:
-                        fullname = os.path.join(root, f)
-                        self.sed(fullname)
+    def start(self):
+        for root, dir, fs in os.walk(self.base):
+            for f in fs:
+                if f[f.rfind(".") + 1:] in self.suffix:
+                    tmp = Audio(os.path.join(root, f), self.delimiter)
+                    try:
+                        self.audios[tmp.Album].append(tmp)
+                    except:
+                        self.audios[tmp.Album] = []
+                        self.audios[tmp.Album].append(tmp)
+
+        for i in self.audios.keys():
+            tmp = []
+            for j in self.audios[i]:
+                tmp.extend(j.Artist)
+            tmp = list(set(tmp))
+
+            if len(tmp) <= self.limit:
+                self.artists[i] = tmp
+            else:
+                self.artists[i] = ["Various Artists"]
+
+        for i in self.audios.keys():
+            for j in self.audios[i]:
+                if j.Album or j.Artist:
+                    self.sed(j)
