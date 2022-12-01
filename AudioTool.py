d10820b2e96f9a9c486cd97d481fdef6a3bb557e
hexin-lin-1024 <2366093667@qq.com>
2022-12-01 12:57:52 +0800

    2022.12.1.d

--- AudioTool.py
+++ AudioTool.py
@@ -1,107 +1,121 @@
-from mutagen.id3 import ID3, TIT2, TPE1, TALB
-from mutagen.flac import FLAC
-import mutagen
+from AUDIO import AUDIO
 import os
 import shutil
 
 
-class Audio:
-    def __init__(self, path, delimiter, cap={"Artist": True, }):
-        self.path = os.path.abspath(path)
+class EAUDIO(AUDIO):
+    def __init__(self, path, delimiter, CAPARTIST=False):
+        super().__init__(path)
         self.delimiter = delimiter
-        self.dir = os.path.dirname(self.path)
-        self.cap = cap
+        self.dir = os.path.dirname(path)
+        self.CAPARTIST = CAPARTIST
         try:
-            audio = FLAC(self.path)
-            audio['ARTIST'] = self.Artist
-            audio.save()
+            audio = AUDIO(path)
+            audio.ARTIST = self.ARTIST
         except:
-            try:
-                tags = ID3(self.path)
-                tags['TPE1'] = TPE1(
-                    encoding=3,
-                    text=self.Artist
-                )
-                tags.save(self.path)
-            except:
-                pass
+            pass
 
     @property
-    def Title(self):
-        title = ''
-        try:
-            file = mutagen.File(self.path)
-            title = file.tags["TIT2"].text[0]
-        except:
-            try:
-                tags = mutagen.File(self.path).tags
-                for i in tags:
-                    if i[0].upper() == 'TITLE':
-                        title = i[1]
-                        break
-            except:
-                pass
-        if self.cap["Title"]:
-            for i in title:
-                i = i.title()
-        return title.strip()
+    def TITLE(self):
+        return super().TITLE[0]
+
+    @TITLE.setter
+    def TITLE(self, Value):
+        self.__generalsetter__("TITLE", Value)
 
     @property
-    def Album(self):
-        album = ''
-        try:
-            file = mutagen.File(self.path)
-            album = file.tags["TALB"].text[0]
-        except:
-            try:
-                tags = mutagen.File(self.path).tags
-                for i in tags:
-                    if i[0].upper() == 'ALBUM':
-                        album = i[1]
-                        break
-            except:
-                pass
-        if self.cap["Album"]:
-            for i in album:
-                i = i.title()
-        return album.strip()
+    def ALBUM(self):
+        return super().ALBUM[0]
+
+    @ALBUM.setter
+    def ALBUM(self, Value):
+        self.__generalsetter__("ALBUM", Value)
 
     @property
-    def Artist(self):
-        artist = []
-        try:
-            file = mutagen.File(self.path)
-            artist = file.tags["TPE1"].text
-        except:
-            try:
-                tags = mutagen.File(self.path).tags
-                for i in tags:
-                    if i[0].upper() == 'ARTIST':
-                        artist.append(i[1])
-            except:
-                pass
+    def ARTIST(self):
+        r = super().ARTIST
         for i in self.delimiter:
             fin = []
-            for j in artist:
+            for j in r:
                 tmp = j.split(i)
                 while '' in tmp:
                     tmp.remove('')
                 for j in range(0, len(tmp)):
                     tmp[j] = tmp[j].strip()
                 fin += tmp
-            artist = fin
-        if self.cap["Artist"]:
-            for i in artist:
+            r = fin
+        if self.CAPARTIST:
+            for i in r:
                 i = i.title()
-        return artist
+        return r
+
+    @ARTIST.setter
+    def ARTIST(self, Value):
+        self.__generalsetter__("ARTIST", Value)
+
+    @property
+    def YEAR(self):
+        return super().YEAR[0]
+
+    @YEAR.setter
+    def YEAR(self, Value):
+        self.__generalsetter__("YEAR", Value)
+
+    @property
+    def TRACK(self):
+        return super().TRACK[0]
+
+    @TRACK.setter
+    def TRACK(self, Value):
+        self.__generalsetter__("TRACK", Value)
+
+    @property
+    def GENRE(self):
+        return super().GENRE[0]
+
+    @GENRE.setter
+    def GENRE(self, Value):
+        self.__generalsetter__("GENRE", Value)
+
+    @property
+    def COMMENT(self):
+        return super().COMMENT[0]
+
+    @COMMENT.setter
+    def COMMENT(self, Value):
+        self.__generalsetter__("COMMENT", Value)
+
+    @property
+    def ALBUMARTIST(self):
+        return super().ALBUMARTIST[0]
+
+    @ALBUMARTIST.setter
+    def ALBUMARTIST(self, Value):
+        self.__generalsetter__("ALBUMARTIST", Value)
+
+    @property
+    def COMPOSER(self):
+        return super().COMPOSER[0]
+
+    @COMPOSER.setter
+    def COMPOSER(self, Value):
+        self.__generalsetter__("COMPOSER", Value)
+
+    @property
+    def DISCNUMBER(self):
+        return super().DISCNUMBER[0]
+
+    @DISCNUMBER.setter
+    def DISCNUMBER(self, Value):
+        self.__generalsetter__("DISCNUMBER", Value)
 
     @property
     def FileName(self):
-        return " - ".join(list(filter(None, [', '.join(self.Artist), self.Album, self.Title])))
+        return " - ".join(list(filter(None, [', '.join(self.ARTIST), self.ALBUM, self.TITLE])))
 
 
 class AudioTool:
-    def __init__(self, base, suffix=["mp3", "flac", "aif"], delimiter=['/', 'ft.', 'feat.', '\\', '\\\\', '、'], limit=8):
+    def __init__(self, base, suffix=["aac", "aif", "aiff", "dsf", "flac", "m4a", "mp3", "ogg", "opus", "wav", "wv", "m4b", "mpc", "ape"], delimiter=['/', 'ft.', 'feat.', '\\', '\\\\', '、'], limit=8):
         self.base = base.strip("/\\")
         self.suffix = suffix
         self.delimiter = delimiter
@@ -118,56 +132,57 @@ class AudioTool:
 
     def sed(self, au):
         dirname = self.fstd(" - ".join(list(filter(None,
-                                                   [', '.join(self.artists[au.Album]), au.Album]))))
-
+                                                   [', '.join(self.artists[au.ALBUM]), au.ALBUM]))))
         fname = au.FileName
         lrcname = fname + '.lrc'
-        fname += os.path.splitext(au.path)[-1]
+        fname += os.path.splitext(au.Path)[-1]
         fname = self.fstd(fname)
 
         if dirname not in os.listdir():
             try:
-                os.mkdir(self.base + "\\" + dirname)
-                print('Successfully Made Dir:' + dirname)
+                if not os.path.exists(self.base + "\\" + dirname):
+                    os.mkdir(self.base + "\\" + dirname)
+                    # print('Successfully Made Dir: ' + dirname)
             except Exception as e:
                 print('Unexpected Error Occurred While Making Dir: ' + dirname)
                 print(e)
 
         try:
-            os.rename(au.path, au.dir + "\\" + fname)
-            # print('Successfully Renamed ' + au.path +
+            os.rename(au.Path, au.dir + "\\" + fname)
+            # print('Successfully Renamed ' + au.Path +
             #   ' as ' + au.dir + "\\" + fname)
         except Exception as e:
-            print('Unexpected Error Occurred While Renaming File: ' + au.path)
+            print('Unexpected Error Occurred While Renaming File: ' + au.Path)
             print(e)
 
         try:
             shutil.move(au.dir + "\\" + fname, self.fjoin(dirname, fname))
-            # print('Successfully Moved Song: ' + au.path)
+            # print('Successfully Moved Song: ' + au.Path)
             if os.path.exists(au.dir + "\\" + lrcname):
                 shutil.move(au.dir + "\\" + lrcname,
                             self.fjoin(dirname, lrcname))
                 # print('Successfully Moved Lrc: ' + lrcname)
         except Exception as e:
-            print('Unexpected Error Occurred While Moiving File: ' + au.path)
+            print('Unexpected Error Occurred While Moiving File: ' + au.Path)
             print(e)
 
     def start(self):
         for root, dir, fs in os.walk(self.base):
             for f in fs:
                 if f[f.rfind(".") + 1:] in self.suffix:
-                    tmp = Audio(os.path.join(root, f), self.delimiter)
+                    tmp = EAUDIO(os.path.join(root, f), self.delimiter)
                     try:
-                        self.audios[tmp.Album].append(tmp)
+                        self.audios[tmp.ALBUM].append(tmp)
                     except:
-                        self.audios[tmp.Album] = []
-                        self.audios[tmp.Album].append(tmp)
+                        self.audios[tmp.ALBUM] = []
+                        self.audios[tmp.ALBUM].append(tmp)
 
         for i in self.audios.keys():
             tmp = []
             for j in self.audios[i]:
-                tmp.extend(j.Artist)
+                tmp.extend(j.ARTIST)
             tmp = list(set(tmp))
+            tmp.sort()
 
             if len(tmp) <= self.limit:
                 self.artists[i] = tmp
@@ -176,5 +191,5 @@ class AudioTool:
 
         for i in self.audios.keys():
             for j in self.audios[i]:
-                if j.Album or j.Artist:
+                if j.ALBUM or j.ARTIST:
                     self.sed(j)
