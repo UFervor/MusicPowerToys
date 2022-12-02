758be809aad697ee9de5c3e0a52e64d2803f7fac
UFervor <micsof365@gmail.com>
2022-12-02 09:55:21 +0800

    2022.12.2.b

--- AUDIO.py
+++ AUDIO.py
@@ -1,6 +1,6 @@
-import mutagen
-# from mutagen.easyid3 import EasyID3
+import os
 import importlib
+import mutagen
 from mutagen.id3 import TIT2, TPE1, TALB, TDRC, TRCK, TCON, COMM, TPE2, TCOM, TPOS, TXXX, USLT
 
 
@@ -345,3 +345,134 @@ class AUDIO:
     # def DICT(self, Value):
     #     for i in Value.keys():
     #         getattr(self, i) = Value[i]
+
+
+class EAUDIO(AUDIO):
+    def __init__(self, path, delimiter, CAPARTIST=False):
+        super().__init__(path)
+        self.delimiter = delimiter
+        self.dir = os.path.dirname(path)
+        self.CAPARTIST = CAPARTIST
+
+    def FORMAT(self, ARTIST=True, ALBUMARTIST=True, COMPOSER=True):
+        if ARTIST:
+            self.ARTIST = self.SEP(self.ARTIST)
+        if ALBUMARTIST:
+            self.ALBUMARTIST = self.SEP(self.ALBUMARTIST)
+        if COMPOSER:
+            self.COMPOSER = self.SEP(self.COMPOSER)
+
+    def SEP(self, Values):
+        r = Values
+        for i in self.delimiter:
+            fin = []
+            for j in r:
+                tmp = list(filter(None, j.split(i)))
+                tmp = [i.strip() for i in tmp]
+                fin += tmp
+            r = fin
+        if self.CAPARTIST:
+            for i in r:
+                i = i.title()
+        return r
+
+    @property
+    def TITLE(self):
+        return super().TITLE[0]
+
+    @TITLE.setter
+    def TITLE(self, Value):
+        self.__generalsetter__("TITLE", Value)
+
+    @property
+    def ALBUM(self):
+        return super().ALBUM[0]
+
+    @ALBUM.setter
+    def ALBUM(self, Value):
+        self.__generalsetter__("ALBUM", Value)
+
+    @property
+    def ARTIST(self):
+        return super().ARTIST
+
+    @ARTIST.setter
+    def ARTIST(self, Value):
+        self.__generalsetter__("ARTIST", Value)
+
+    @property
+    def YEAR(self):
+        return super().YEAR[0]
+
+    @YEAR.setter
+    def YEAR(self, Value):
+        self.__generalsetter__("YEAR", Value)
+
+    @property
+    def TRACK(self):
+        return super().TRACK[0]
+
+    @TRACK.setter
+    def TRACK(self, Value):
+        self.__generalsetter__("TRACK", Value)
+
+    @property
+    def GENRE(self):
+        return super().GENRE[0]
+
+    @GENRE.setter
+    def GENRE(self, Value):
+        self.__generalsetter__("GENRE", Value)
+
+    @property
+    def COMMENT(self):
+        return super().COMMENT[0]
+
+    @COMMENT.setter
+    def COMMENT(self, Value):
+        self.__generalsetter__("COMMENT", Value)
+
+    @property
+    def ALBUMARTIST(self):
+        return super().ALBUMARTIST
+
+    @ALBUMARTIST.setter
+    def ALBUMARTIST(self, Value):
+        self.__generalsetter__("ALBUMARTIST", Value)
+
+    @property
+    def COMPOSER(self):
+        return super().COMPOSER
+
+    @COMPOSER.setter
+    def COMPOSER(self, Value):
+        self.__generalsetter__("COMPOSER", Value)
+
+    @property
+    def DISCNUMBER(self):
+        return super().DISCNUMBER[0]
+
+    @DISCNUMBER.setter
+    def DISCNUMBER(self, Value):
+        self.__generalsetter__("DISCNUMBER", Value)
+
+    @property
+    def LYRICS(self):
+        return super().LYRICS[0]
+
+    @LYRICS.setter
+    def LYRICS(self, Value):
+        self.__generalsetter__("LYRICS", Value)
+
+    def ADDLYRICS(self, FO=None, String=None):
+        if String:
+            pass
+        elif FO:
+            String = FO.read()
+        else:
+            raise ValueError("No args are provided to attach lyrics")
+        self.LYRICS = String
+
+    @property
+    def FileName(self):
+        return " - ".join(list(filter(None, [', '.join(self.ARTIST), self.ALBUM, self.TITLE])))
